40c1519748abbc0816de9c1ef6ba2bebf347ce14
Gabriel Dias <ti.gabriel.dias@gmail.com>
2020-04-12 15:17:26 -0300

    Removido o markup de itens do carrinho, ainda está estático no js, mas pronto para ser tratado para renderizar dinamicamente. Adicionado o obj carrinhoItens que irá representar a lista de itens dinâmica, um obj normalizado onde a chave é seu id, facilitando e evitando varrer um array para procurar sua existência em determinada coleção.

--- componentes/main.js
+++ componentes/main.js
@@ -22,6 +22,27 @@ const produtos = [
     }
 ];
 
+const carrinhoItens = {
+
+    'bbc123': {
+        id: 'bbc123',
+        nome: 'JSRaiz para Node',
+        preco: 1200,
+        descricao: 'O melhor curso de todos',
+        imagem: 'http://lorempixel.com/400/200' 
+    },
+
+    'cbc123': {
+        id: 'cbc123',
+        nome: 'Programação funcional com JS',
+        preco: 500,
+        descricao: 'O melhor funcional de todos',
+        imagem: 'http://lorempixel.com/400/200' 
+    }
+}
+
+console.log(carrinhoItens)
+
 function renderizaProduto(produto){
     return `
         <div class="col-sm-4 mb-3">
@@ -44,4 +65,18 @@ function renderizaProdutos(){
     return produtos.map(produto => renderizaProduto(produto)).join('');
 };
 
+function renderizaCarrinho(){
+    return `
+        <div class="card carrinho__item">
+            <div class="card-body">
+                <h5 class="card-title">JSRaiz para FW</h5>
+                <p class="card-text">Preço unidade: R$300,00 | Quantidade: 2</p>
+                <p class="card-text">Valor: R$600,00</p>
+                <button href="#" data-value="300" class="btn btn-danger btn-sm">Adicionar</button>
+            </div>
+        </div>
+    `
+}
+
 document.querySelector('.loja').innerHTML = renderizaProdutos();
+document.querySelector('.carrinho__itens').innerHTML = renderizaCarrinho();
