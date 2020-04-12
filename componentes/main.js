29128aa953e96c9280c374b5f288aef71f1d65c0
Gabriel Dias <ti.gabriel.dias@gmail.com>
2020-04-12 16:56:48 -0300

    Removido o mock do obj carrinhoItens, os itens agora são adicionados dinamicamente no evento adicinado no body da página, para que sempre no clique do botão adicionar o carrinho seja renderizado e inserido no html

--- componentes/main.js
+++ componentes/main.js
@@ -22,29 +22,10 @@ const produtos = [
     }
 ];
 
-const carrinhoItens = {
-
-    'bbc123': {
-        id: 'bbc123',
-        nome: 'JSRaiz para Node',
-        preco: 1200,
-        quantidade: 1,
-        descricao: 'O melhor curso de todos',
-        imagem: 'http://lorempixel.com/400/200' 
-    },
-
-    'cbc123': {
-        id: 'cbc123',
-        nome: 'Programação funcional com JS',
-        preco: 500,
-        quantidade: 2,
-        descricao: 'O melhor funcional de todos',
-        imagem: 'http://lorempixel.com/400/200' 
-    }
-}
+const carrinhoItens = {};
 
 
-function renderizaProduto(produto){
+function renderizaProduto(produto, index){
     return `
         <div class="col-sm-4 mb-3">
             <div class="card">
@@ -54,7 +35,7 @@ function renderizaProduto(produto){
                         <h5 class="card-title">${produto.nome}</h5>
                         <small>R$${produto.preco},00</small>
                         <p class="card-text">${produto.descricao}</p>
-                        <button href="#" data-value="300" class="btn btn-primary">Adicionar</button>
+                        <button data-index="${index}" href="#" data-value="300" class="btn btn-primary btn-add">Adicionar</button>
                     </div>
                 </div>
             </div>
@@ -63,7 +44,7 @@ function renderizaProduto(produto){
 };
 
 function renderizaProdutos(){
-    return produtos.map(produto => renderizaProduto(produto)).join('');
+    return produtos.map((produto, ind) => renderizaProduto(produto, ind),).join('');
 };
 
 function renderizaItemCarrinho(item){
@@ -73,7 +54,7 @@ function renderizaItemCarrinho(item){
                 <h5 class="card-title">${item.nome}</h5>
                 <p class="card-text">Preço unidade: R$${item.preco},00 | Quantidade: ${item.quantidade}</p>
                 <p class="card-text">Valor: R$${item.preco * item.quantidade},00</p>
-                <button href="#" data-value="300" class="btn btn-danger btn-sm">Adicionar</button>
+                <button href="#" data-value="300" class="btn btn-danger btn-sm">Remover</button>
             </div>
         </div>
     `
@@ -84,9 +65,21 @@ function renderizaCarrinho(){
     for(produtoId in carrinhoItens){
         html += renderizaItemCarrinho(carrinhoItens[produtoId]);
     }
-    return html;
+    document.querySelector('.carrinho__itens').innerHTML = html;
 }
 
+document.body.addEventListener('click', (event) =>{
+    if(event.target.classList.contains('btn-add')){
+        const index = event.target.getAttribute('data-index');
+        const produto = produtos[event.target.getAttribute('data-index')];
+
+        if(!carrinhoItens[produto.id]){
+            carrinhoItens[produto.id] = produto;
+            carrinhoItens[produto.id].quantidade = 1;          
+        } 
+
+        renderizaCarrinho();
+    }
+})
+
 document.querySelector('.loja').innerHTML = renderizaProdutos();
-document.querySelector('.carrinho__itens').innerHTML = renderizaCarrinho();
-renderizaCarrinho()
