fe645e43aaf63da02d5ecd6fa2b81ecbcf2274b1
Gabriel Dias <ti.gabriel.dias@gmail.com>
2020-04-12 21:08:14 -0300

    A partir de agora, transformar o que foi feito utilizando vanilla para React. Criado os componentes iniciais com createElement do React

---
+++ componentes-react/main-react.js
@@ -0,0 +1,43 @@
+function AppComponente(){
+    return (
+        React.createElement(React.Fragment, null,
+            React.createElement('div', { className: 'col-sm-8'}, 
+                React.createElement('div', { className: 'row loja'},
+                    React.createElement('div', { className: 'col-sm-4 mb-3'},
+                        React.createElement('div', { className: 'card loja__item'},
+                            React.createElement('img', { className: 'card-img-top', src:"http://lorempixel.com/400/200"}),
+                            React.createElement('div', { className: 'card-body' },
+                                React.createElement('h5', { className: 'card-title'}, 'JSRaiz para FW'),
+                                React.createElement('small', null, 'R$300,00'),
+                                React.createElement('p', { className: "card-text" }, 'O melhor curso do mundo'),
+                                React.createElement('button', { className: "btn btn-primary" }, 'Adicionar')
+                            )   
+                        )
+                    )
+                )
+            )
+        ,
+        React.createElement('div', { className: 'col-sm-4'}, 
+            React.createElement('div', { className: 'carrinho'},
+            React.createElement('div', { className: 'carrinho__itens'},
+                React.createElement('div', { className: 'card carrinho__item'},
+                    React.createElement('div', { className: 'card-body'},
+                        React.createElement('h5', { className: 'card-title'}, 'JSRaiz para FW'),
+                        React.createElement('p', { className: "card-text" }, 'Preço unidade: R$300,00 | Quantidade: 2'),
+                        React.createElement('button', { className: "btn btn-danger btn-sm" }, 'Remover')
+                    )
+                )
+            ),
+            React.createElement('div', { className: 'carrinho__total mt-2 p-3"'}),
+                React.createElement('h6', null, 'Total:'),
+                    React.createElement('strong', null, 'R$600,00')
+                )
+            )   
+        )
+    )
+}
+
+ReactDOM.render(
+    React.createElement(AppComponente),
+    document.getElementById('app')
+)
